5b8a3993d9f336f95999ee179615f1111fdde281
yuebuqunzz <119918362+yuebuqunzz@users.noreply.github.com>
2022-12-07 10:15:30 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-hix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
