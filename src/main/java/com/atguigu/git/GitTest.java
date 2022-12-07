0fe54df7ec3bdf26cf399a73306b9d8aab5100c9
ZC <ZC@atguigu.com>
2022-12-07 09:08:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ZC
+ * @create 2022-12-07 9:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
